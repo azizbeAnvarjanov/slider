6a3764f22b3d289bbcb5c7c45f4345e316fe8d80
azizbeAnvarjanov <https://github.com/azizbeAnvarjanov>
2022-11-22 11:33:46 +0500

    ededw

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+
+
+// Default theme
+import '@splidejs/react-splide/css';
+
+// or other themes
+import '@splidejs/react-splide/css/skyblue';
+import '@splidejs/react-splide/css/sea-green';
+
+// or only core styles
+import '@splidejs/react-splide/css/core';
+
+
+import Img1 from './imgd/1.jpg'
+import Img2 from './imgd/2.jpg'
+import Img3 from './imgd/3.jpg'
+import Img4 from './imgd/4.jpg'
+import Img5 from './imgd/5.jpg'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+   <>
+      <header>
+        <Splide
+        className="slider"
+        aria-label="My Favorite Images"
+          options={{
+            type: 'loop',
+            width: "auto",
+            height: "100%",
+            perPage: 2,
+            breakpoints: {
+              640: {
+                perPage: 2,
+              },
+            },
+            autoplay: true,
+            focus: 'center',
+            gap: '1rem',
+          }}
         >
-          Learn React
-        </a>
+          <SplideSlide className="slide">
+            <img src={Img1} alt="" />
+          </SplideSlide>
+          <SplideSlide className="slide">
+            <img src={Img2} alt="" />
+          </SplideSlide>
+          <SplideSlide className="slide">
+            <img src={Img3} alt="" />
+          </SplideSlide>
+          <SplideSlide className="slide">
+            <img src={Img4} alt="" />
+          </SplideSlide>
+          <SplideSlide className="slide">
+            <img src={Img5} alt="" />
+          </SplideSlide>
+        </Splide>
       </header>
-    </div>
+   </>
   );
 }
 
